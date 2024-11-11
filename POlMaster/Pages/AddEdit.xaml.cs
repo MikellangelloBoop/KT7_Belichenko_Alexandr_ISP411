07e861a8dc5c24047e46b723b017777de6026ca7
MikellangelloBoop <belichenko24.08@mail.ru>
2024-11-11 16:30:36 +0300

    Добавьте файлы проекта.

---
+++ POlMaster/Pages/AddEdit.xaml.cs
@@ -0,0 +1,43 @@
+﻿using POlMaster.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace POlMaster.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEdit.xaml
+    /// </summary>
+    public partial class AddEdit : Page
+    {
+
+
+        public AddEdit(PartnersImport partner)
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            Classes.Manager.MainFrame.Navigate(new Pages.ListView());
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
